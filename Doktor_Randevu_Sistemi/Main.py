58c173cfd0faa60eef5d0f03134052592f229824
qwe12e <asd@asd.ads>
2017-12-12 13:20:29 +0300

    lkik

---
+++ Doktor_Randevu_Sistemi/Main.py
@@ -0,0 +1,15 @@
+class Kullanici:
+    def __init__(self, ad, soyad, id, password):
+        self.ad = ad
+        self.soyad = soyad
+        self.id = id
+        self.password = password
+
+class Doktor(Kullanici):
+    def __init__(self, ad, soyad, id, password):
+        self.randevu = []
+        super(Doktor, self).__init__(ad, soyad, id, password)
+
+class Hasta(Kullanici):
+    def __init__(self, ad, soyad, id, password):
+        super(Kullanici, self).__init__(ad, soyad, id, password)
